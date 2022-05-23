272778c6a63db01451f542b474bd5f03c07c919f
andika4140 <goku200200>
2022-05-23 17:02:28 +0700

    menambahkan gallery dan memperbaiki layout option

--- app/src/main/java/com/example/sa_lindungi/UI/scanAnimal/ResultActivity.kt
+++ app/src/main/java/com/example/sa_lindungi/UI/scanAnimal/ResultActivity.kt
@@ -1,8 +1,11 @@
 package com.example.sa_lindungi.UI.scanAnimal
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.example.sa_lindungi.UI.home.MainActivity
 import com.example.sa_lindungi.databinding.ActivityResultBinding
 
@@ -14,7 +17,22 @@ class ResultActivity : AppCompatActivity() {
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupView()
+
         binding.buttonHome.setOnClickListener { toHome() }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 
     private fun toHome() {
